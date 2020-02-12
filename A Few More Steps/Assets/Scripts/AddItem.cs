1a999a4950e86719f1db0ce6f17298a19adabbb3
Gabriel P <gaberofl42@gmail.com>
2020-02-13 10:35:12 +1100

    Additions
    Added inventory mechaninc which adds picked up items into a list

---
+++ A Few More Steps/Assets/Scripts/AddItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddItem : MonoBehaviour
+{
+    public float distance;
+    public LayerMask mask;
+    private GameObject inventory;
+    private void Start()
+    {
+        inventory = GameObject.Find("Inventory");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward,out hit, distance, mask) && !PlayerMovement.stopMovement)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                inventory.GetComponent<Inventory>().inventory(hit.transform.gameObject.name);
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
---
+++ A Few More Steps/Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<GameObject> collectables;
+    private void Start()
+    {
+        // Get saved list 
+        // check inventory at list(i)
+        //// if list i == item number unlock item
+       
+        //for (int i = 0; i < collectables.Count; i++)
+        //{
+
+        //}
+    }
+    public void inventory(string itemName)
+    {
+      
+        switch (itemName)
+        {
+            case "A":
+                collectables[0].SetActive(true);
+                break;
+            case "B":
+                collectables[1].SetActive(true);
+                break;
+            case "C":
+                collectables[2].SetActive(true);
+                break;
+        }
+    }
+}
