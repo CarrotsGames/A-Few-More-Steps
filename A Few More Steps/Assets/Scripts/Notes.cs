26d6d3b74cffdb2c530f4f26ce00274aa70f954d
Gabriel4224 <31612343+Gabriel4224@users.noreply.github.com>
2020-02-26 11:31:26 +1100

    Began implementing note system

---
+++ A Few More Steps/Assets/Scripts/Notes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    public GameObject noteUi;
+    public Text noteText;
+    public void ReadNote()
+    {
+        noteUi.transform.GetChild(0).GetComponent<Text>().text = noteText.text;
+        noteUi.SetActive(true);
+    }
+}
---
+++ A Few More Steps/Assets/Scripts/ReadScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Equips to cameraCM
+public class ReadScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        { 
+            if (hit.transform.tag == "Book")
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
